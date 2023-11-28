1a1dfcff50ce3595cc83b35fe54894c807bd0cf1
Stefan <stefan.naeve@hotmail.com>
2023-11-28 13:07:59 +0100

    Initial commit

---
+++ src/main/java/com/example/examinbackend/ExamInBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.examinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamInBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamInBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examinbackend/ExamInBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamInBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
