d770551320a23a86da7b42f64b09e18ef4ae3d30
Stefan <stefan.naeve@hotmail.com>
2023-11-28 15:29:39 +0100

    Made all fields in Customer.java

--- src/main/java/com/example/examinbackend/model/Customer.java
+++ src/main/java/com/example/examinbackend/model/Customer.java
@@ -1,4 +1,45 @@
 package com.example.examinbackend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "customer_generator")
+    @SequenceGenerator(name = "customer_generator", sequenceName = "customer_seq", initialValue = 1, allocationSize = 1)
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("customer_id")
+    @JoinColumn(name = "customer_id")
+    private List<Addresses> addresses = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("customer_id")
+    @JoinColumn(name = "customer_id")
+    private List<Orders> orders = new ArrayList<>();
+
 }
