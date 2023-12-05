ec9295af7b41478dcd8fd7b1a590a8b68e2f2edd
Stefan <stefan.naeve@hotmail.com>
2023-12-05 16:00:23 +0100

    Filled customerController with get and post

--- src/main/java/com/example/examinbackend/controller/CustomerController.java
+++ src/main/java/com/example/examinbackend/controller/CustomerController.java
@@ -1,4 +1,37 @@
 package com.example.examinbackend.controller;
 
+import com.example.examinbackend.model.Customer;
+import com.example.examinbackend.service.CustomerService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomer(HttpServletRequest request, @PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+
 }
