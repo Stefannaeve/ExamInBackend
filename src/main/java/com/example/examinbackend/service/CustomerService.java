c118816dfe7ae68353824fc5a577036963e84af8
Stefan <stefan.naeve@hotmail.com>
2023-12-05 16:01:00 +0100

    Made services in the customerService

--- src/main/java/com/example/examinbackend/service/CustomerService.java
+++ src/main/java/com/example/examinbackend/service/CustomerService.java
@@ -1,4 +1,31 @@
 package com.example.examinbackend.service;
 
+import com.example.examinbackend.model.Customer;
+import com.example.examinbackend.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
 }
