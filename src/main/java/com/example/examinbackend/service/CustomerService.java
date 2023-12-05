4352024c19707d20c4b1dbadba22c4e32659a1b9
Stefan <stefan.naeve@hotmail.com>
2023-12-05 19:04:31 +0100

    Made updating customer

--- src/main/java/com/example/examinbackend/service/CustomerService.java
+++ src/main/java/com/example/examinbackend/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.examinbackend.service;
 
+import com.example.examinbackend.model.Address;
 import com.example.examinbackend.model.Customer;
 import com.example.examinbackend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,27 @@ public class CustomerService {
 
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    public Customer updateCustomerName(Long id, Customer customer) {
+        Customer existingCustomer = getCustomerById(id);
+        existingCustomer.setCustomerName(customer.getCustomerName());
+        return customerRepository.save(existingCustomer);
+    }
+
+    public Customer updateCustomerEmail(Long id, Customer customer) {
+        Customer existingCustomer = getCustomerById(id);
+        existingCustomer.setEmail(customer.getEmail());
+        return customerRepository.save(existingCustomer);
+    }
+
+    public Customer updateCustomerPhone(Long id, Customer customer) {
+        Customer existingCustomer = getCustomerById(id);
+        existingCustomer.setPhone(customer.getPhone());
+        return customerRepository.save(existingCustomer);
     }
 }
