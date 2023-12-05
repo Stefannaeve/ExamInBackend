89b2873a6bd1b25036ef49b10f3813ace3d444cc
Stefan <stefan.naeve@hotmail.com>
2023-12-05 19:04:15 +0100

    Made it so you can add Address to customer.

--- src/main/java/com/example/examinbackend/controller/CustomerController.java
+++ src/main/java/com/example/examinbackend/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.example.examinbackend.controller;
 
+import com.example.examinbackend.model.Address;
 import com.example.examinbackend.model.Customer;
+import com.example.examinbackend.service.AddressService;
 import com.example.examinbackend.service.CustomerService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +14,12 @@ import java.util.List;
 @RequestMapping("/api/customer")
 public class CustomerController {
     private final CustomerService customerService;
+    private final AddressService addressService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, AddressService addressService) {
         this.customerService = customerService;
+        this.addressService = addressService;
     }
 
     @GetMapping("/{id}")
@@ -28,9 +32,38 @@ public class CustomerController {
         return customerService.getAllCustomers();
     }
 
-    @PostMapping
+    @PostMapping("/add")
     public Customer createCustomer(@RequestBody Customer customer) {
         return customerService.createCustomer(customer);
+    }
+
+    @PostMapping("/add/{customerId}/{addressId}")
+    public void addCustomerAddress(@PathVariable Long customerId, @PathVariable Long addressId) {
+        Customer customer = customerService.getCustomerById(customerId);
+        Address address = addressService.getAddressById(addressId);
+
+        customer.getAddresses().add(address);
+        customerService.createCustomer(customer);
+    }
+
+    @PutMapping("/update/{id}")
+    public Customer updateCustomerName(@PathVariable Long id, @RequestBody Customer customer) {
+        return customerService.updateCustomerName(id, customer);
+    }
+
+    @PutMapping("/update/{id}/email")
+    public Customer updateCustomerEmail(@PathVariable Long id, @RequestBody Customer customer) {
+        return customerService.updateCustomerEmail(id, customer);
+    }
+
+    @PutMapping("/update/{id}/phone")
+    public Customer updateCustomerPhone(@PathVariable Long id, @RequestBody Customer customer) {
+        return customerService.updateCustomerPhone(id, customer);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
     }
 
 
