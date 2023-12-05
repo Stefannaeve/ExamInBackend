c4ac86adc3f95a2b904fb2ea7a005465ae3cb12a
Stefan <stefan.naeve@hotmail.com>
2023-12-05 16:01:46 +0100

    Made subassembly class and fields

---
+++ src/main/java/com/example/examinbackend/model/Subassembly.java
@@ -0,0 +1,30 @@
+package com.example.examinbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+public class Subassembly {
+
+    @Id
+    @GeneratedValue(generator = "subassembly_generator")
+    @SequenceGenerator(name = "subassembly_generator", sequenceName = "subassembly_seq", initialValue = 1, allocationSize = 1)
+    @Column(name = "subassembly_id")
+    private Long id;
+
+    @Column(name = "subassembly_name")
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "part_id")
+    private List<Part> parts;
+}
