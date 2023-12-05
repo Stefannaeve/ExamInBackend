f2c66db30ae1b2c7ca6bbe5d16428aded35b2d29
Stefan <stefan.naeve@hotmail.com>
2023-12-05 16:00:39 +0100

    Made the customerRepo extend

--- src/main/java/com/example/examinbackend/repository/CustomerRepository.java
+++ src/main/java/com/example/examinbackend/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.example.examinbackend.repository;
 
-public interface CustomerRepository {
+import com.example.examinbackend.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
 }
