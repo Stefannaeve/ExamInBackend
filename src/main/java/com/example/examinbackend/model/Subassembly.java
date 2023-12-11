15f5c71bfaad81ff4556cc41bd8a4f4fc188f728
Stefan <stefan.naeve@hotmail.com>
2023-12-11 21:23:22 +0100

    Found the problem in the Machine and subassembly long id, Should work now

--- src/main/java/com/example/examinbackend/model/Subassembly.java
+++ src/main/java/com/example/examinbackend/model/Subassembly.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,14 +22,15 @@ public class Subassembly {
     @GeneratedValue(generator = "subassembly_generator")
     @SequenceGenerator(name = "subassembly_generator", sequenceName = "subassembly_seq", initialValue = 1, allocationSize = 1)
     @Column(name = "subassembly_id")
-    private Long id;
+    private Long id = 0L;
 
     @Column(name = "subassembly_name")
     private String subassemblyName;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "subassembly_id")
-    private List<Part> parts;
+    @JsonIgnoreProperties("subassemblies")
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "subassembly_part", joinColumns = {@JoinColumn(name = "subassembly_id")})
+    private List<Part> parts = new ArrayList<>();
 
     public Subassembly(String subassemblyName, List<Part> parts) {
         this.subassemblyName = subassemblyName;
