30eeb5c2f99f63a2644a97ac84a9c61d58747f4a
Stefan <stefan.naeve@hotmail.com>
2023-12-11 22:15:29 +0100

    Updating machines to order in service

--- src/main/java/com/example/examinbackend/service/OrderService.java
+++ src/main/java/com/example/examinbackend/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.examinbackend.service;
 
+import com.example.examinbackend.model.Customer;
 import com.example.examinbackend.model.Order;
 import com.example.examinbackend.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,22 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final CustomerService customerService;
+    private final MachineService machineService;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, CustomerService customerService, MachineService machineService) {
         this.orderRepository = orderRepository;
+        this.customerService = customerService;
+        this.machineService = machineService;
     }
 
     public Order getOrderById(Long id) {
         return orderRepository.findById(id).orElse(null);
     }
-    public Order createOrder(Order order) {
+    public Order createOrder(Order order, Long customerId) {
+        Customer customer = customerService.getCustomerById(customerId);
+        order.setCustomer(customer);
         return orderRepository.save(order);
     }
     public List<Order> getAllOrders() {
@@ -28,5 +35,11 @@ public class OrderService {
     }
     public void deleteOrder(Long id) {
         orderRepository.deleteById(id);
+    }
+
+    public Order updateOrderMachines(Long machineId, Long orderId) {
+        Order order = getOrderById(orderId);
+        order.getMachines().add(machineService.getMachineById(machineId));
+        return orderRepository.save(order);
     }
 }
