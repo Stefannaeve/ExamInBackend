d4b9d8db42d68efba6f82d0190b38e4556006627
Stefan <stefan.naeve@hotmail.com>
2023-12-12 23:30:03 +0100

    Changed subassembly repo method to return optional instead of subassembly

--- src/main/java/com/example/examinbackend/repository/SubassemblyRepository.java
+++ src/main/java/com/example/examinbackend/repository/SubassemblyRepository.java
@@ -4,7 +4,9 @@ import com.example.examinbackend.model.Subassembly;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubassemblyRepository extends JpaRepository<Subassembly, Long>{
-    Subassembly findBySubassemblyName(String name);
+    Optional<Subassembly> findBySubassemblyName(String name);
 }
