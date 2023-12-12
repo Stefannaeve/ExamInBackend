10908fd582f1b86b31cde2a63d718d0e8d0fd26a
Stefan <stefan.naeve@hotmail.com>
2023-12-12 01:21:38 +0100

    Started doing httpStatus and optional

--- src/main/java/com/example/examinbackend/service/OrderService.java
+++ src/main/java/com/example/examinbackend/service/OrderService.java
@@ -1,12 +1,14 @@
 package com.example.examinbackend.service;
 
 import com.example.examinbackend.model.Customer;
+import com.example.examinbackend.model.Machine;
 import com.example.examinbackend.model.Order;
 import com.example.examinbackend.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -25,21 +27,31 @@ public class OrderService {
     public Order getOrderById(Long id) {
         return orderRepository.findById(id).orElse(null);
     }
-    public Order createOrder(Order order, Long customerId) {
-        Customer customer = customerService.getCustomerById(customerId);
-        order.setCustomer(customer);
-        return orderRepository.save(order);
+
+    public Optional<Order> createOrder(Long customerId) {
+        Order order = new Order();
+        Optional<Customer> optionalCustomer = customerService.getCustomerById(customerId);
+        if (optionalCustomer.isEmpty()) {
+            return Optional.empty();
+        }
+        order.setCustomer(optionalCustomer.get());
+        return Optional.of(orderRepository.save(order));
     }
+
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
     }
+
     public void deleteOrder(Long id) {
         orderRepository.deleteById(id);
     }
 
-    public Order updateOrderMachines(Long machineId, Long orderId) {
+    public Order updateOrderMachines(List<Machine> machines, Long orderId) {
         Order order = getOrderById(orderId);
-        order.getMachines().add(machineService.getMachineById(machineId));
+        for (Machine machine : machines) {
+            machine = machineService.getMachineById(machine.getId());
+            order.getMachines().add(machine);
+        }
         return orderRepository.save(order);
     }
 }
