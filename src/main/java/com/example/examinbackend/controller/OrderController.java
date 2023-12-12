0537f1aa093258a016bbc5e5d84fe4a966b7a308
Stefan <stefan.naeve@hotmail.com>
2023-12-12 01:21:24 +0100

    Started doing httpStatus

--- src/main/java/com/example/examinbackend/controller/OrderController.java
+++ src/main/java/com/example/examinbackend/controller/OrderController.java
@@ -1,13 +1,19 @@
 package com.example.examinbackend.controller;
 
+import com.example.examinbackend.model.Machine;
 import com.example.examinbackend.model.Order;
 import com.example.examinbackend.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/order")
+@Slf4j
 public class OrderController {
     private final OrderService orderService;
 
@@ -19,18 +25,27 @@ public class OrderController {
     public Order getOrder(@PathVariable Long id) {
         return orderService.getOrderById(id);
     }
+
     @GetMapping("/all")
     public List<Order> getAllOrders() {
         return orderService.getAllOrders();
     }
+
     @PostMapping("/add/customer/{customerId}")
-    public Order createOrder(@RequestBody Order order, @PathVariable Long customerId) {
-        return orderService.createOrder(order, customerId);
+    public ResponseEntity<Order> createOrder(@PathVariable Long customerId) {
+        Optional<Order> optionalOrder = orderService.createOrder(customerId);
+        if (optionalOrder.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CREATED).body(optionalOrder.get());
+        }
     }
-    @PostMapping("/update/{orderId}/machines/{machineId}")
-    public Order updateOrderMachines(@PathVariable Long orderId, @PathVariable Long machineId) {
-        return orderService.updateOrderMachines(machineId, orderId);
+
+    @PostMapping("/update/{orderId}")
+    public Order updateOrderMachines(@PathVariable Long orderId, @RequestBody List<Machine> machines) {
+        return orderService.updateOrderMachines(machines, orderId);
     }
+
     @DeleteMapping("/delete/{id}")
     public void deleteOrder(@PathVariable Long id) {
         orderService.deleteOrder(id);
