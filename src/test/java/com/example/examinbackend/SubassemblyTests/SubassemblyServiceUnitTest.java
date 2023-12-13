dab33453283230e0ca968042072c60b8ec050afa
william vage <william.vage@gmail.com>
2023-12-13 15:39:43 +0100

    imported Optional to SubassemblyServiceUnitTest

--- src/test/java/com/example/examinbackend/SubassemblyTests/SubassemblyServiceUnitTest.java
+++ src/test/java/com/example/examinbackend/SubassemblyTests/SubassemblyServiceUnitTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
@@ -27,7 +28,7 @@ public class SubassemblyServiceUnitTest {
         List<Part> parts = List.of(new Part(), new Part());
         Subassembly subassemblyOne = new Subassembly("Subassembly 1", parts);
         Subassembly subassemblyTwo = new Subassembly("Subassembly 2", parts);
-        when(subassemblyRepository.findById(2L)).thenReturn(java.util.Optional.of(subassemblyTwo));
+        when(subassemblyRepository.findById(2L)).thenReturn(Optional.of(subassemblyTwo));
         var foundSubassembly = subassemblyService.getSubassemblyById(2L);
         assert foundSubassembly.get().getSubassemblyName().equals("Subassembly 2");
     }
