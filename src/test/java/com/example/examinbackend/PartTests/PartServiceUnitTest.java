596f113415e040cf66f5bf4bc9658a781f9a02b3
Stefan <stefan.naeve@hotmail.com>
2023-12-13 22:57:44 +0100

    Made profile test in testing

--- src/test/java/com/example/examinbackend/PartTests/PartServiceUnitTest.java
+++ src/test/java/com/example/examinbackend/PartTests/PartServiceUnitTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class PartServiceUnitTest {
 
     @MockBean
@@ -23,28 +26,32 @@ public class PartServiceUnitTest {
 
     @Test
     void shouldGetPartById() {
-        Part partOne = new Part("Part 1");
-        Part partTwo = new Part("Part 2");
-        when(partRepository.findById(2L)).thenReturn(java.util.Optional.of(partTwo));
-        var partById = partService.getPartById(2L);
-        assert partById.get().getPartName().equals("Part 2");
+        List<Part> listOfParts = List.of(new Part("Part 1", 200L), new Part("Part 2", 300L));
+
+        when(partRepository.findById(2L)).thenReturn(Optional.of(listOfParts.get(1)));
+
+        Optional<Part> partById = partService.getPartById(2L);
+
+        Part part = partById.get();
+
+        assert part.getPartName().equals("Part 2");
     }
     @Test
     void shouldCreateANewPart() {
-        Part part = new Part("Part 1");
+        Part part = new Part("Part 1", 200L);
         when(partRepository.save(part)).thenReturn(part);
         var createdPart = partService.createPart(part);
         assert createdPart.getPartName().equals("Part 1");
     }
     @Test
     void shouldDeleteANewPart() {
-        Part part = new Part("Part 1");
+        Part part = new Part("Part 1", 200L);
         partService.deletePart(1L);
         assert partService.getAllParts().size() == 0;
     }
     @Test
     void shouldUpdatePartName() {
-        Part part = new Part("Part 1");
+        Part part = new Part("Part 1", 200L);
         when(partRepository.save(part)).thenReturn(part);
         var createdPart = partService.createPart(part);
         createdPart.setPartName("Part 2");
