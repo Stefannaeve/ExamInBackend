99ca69eb16f58a6e7438e8c7f3784e3363ec4dca
william vage <william.vage@gmail.com>
2023-12-15 20:14:38 +0100

    Added @Repository annotation to AddressRepository

--- src/main/java/com/example/examinbackend/repository/AddressRepository.java
+++ src/main/java/com/example/examinbackend/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.example.examinbackend.repository;
 
 import com.example.examinbackend.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
