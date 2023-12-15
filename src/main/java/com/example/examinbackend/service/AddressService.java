4b3a30b493a5133e04af7cd8b7af27f1f55b210d
william vage <william.vage@gmail.com>
2023-12-15 20:16:10 +0100

    Changed @Repository annotation to @Service, in AddressService.

--- src/main/java/com/example/examinbackend/service/AddressService.java
+++ src/main/java/com/example/examinbackend/service/AddressService.java
@@ -4,12 +4,12 @@ import com.example.examinbackend.model.Address;
 import com.example.examinbackend.model.Customer;
 import com.example.examinbackend.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Service
 public class AddressService {
 
     private final AddressRepository addressRepository;
