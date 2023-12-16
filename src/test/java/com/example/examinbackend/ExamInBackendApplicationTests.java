f84420e943a37423b481cdf945fabdd697f8a58e
william vage <william.vage@gmail.com>
2023-12-16 13:54:13 +0100

    Added ActiveProfiles annotation in ExamInBackendApplicationTests

--- src/test/java/com/example/examinbackend/ExamInBackendApplicationTests.java
+++ src/test/java/com/example/examinbackend/ExamInBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.examinbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExamInBackendApplicationTests {
 
 	@Test
