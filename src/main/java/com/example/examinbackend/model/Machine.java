ffb6133a1d616d34d84ab98bde7f1adf3a1a22c2
william vage <william.vage@gmail.com>
2023-12-16 16:05:22 +0100

    Removed unused import in Machine model.

--- src/main/java/com/example/examinbackend/model/Machine.java
+++ src/main/java/com/example/examinbackend/model/Machine.java
@@ -1,6 +1,5 @@
 package com.example.examinbackend.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
