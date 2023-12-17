cc6a9dd2b7435e19b1759e421ef933ef06f51b35
william vage <william.vage@gmail.com>
2023-12-17 22:11:34 +0100

    Removed unused imports from partService.

--- src/main/java/com/example/examinbackend/service/PartService.java
+++ src/main/java/com/example/examinbackend/service/PartService.java
@@ -4,9 +4,7 @@ import com.example.examinbackend.model.Part;
 import com.example.examinbackend.repository.PartRepository;
 import org.springframework.stereotype.Service;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
