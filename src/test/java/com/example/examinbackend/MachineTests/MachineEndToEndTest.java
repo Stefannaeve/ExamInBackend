890817769de5bf39b528d8544246744ac07b53a6
Stefan <stefan.naeve@hotmail.com>
2023-12-17 15:01:41 +0100

    Made create machine in MachineEndToEndTest

--- src/test/java/com/example/examinbackend/MachineTests/MachineEndToEndTest.java
+++ src/test/java/com/example/examinbackend/MachineTests/MachineEndToEndTest.java
@@ -9,8 +9,10 @@ import com.example.examinbackend.repository.PartRepository;
 import com.example.examinbackend.repository.SubassemblyRepository;
 import com.example.examinbackend.service.AddressService;
 import com.example.examinbackend.service.MachineService;
+import com.example.examinbackend.service.SubassemblyService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,8 +26,10 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -46,6 +50,9 @@ public class MachineEndToEndTest {
 
     @Autowired
     private PartRepository partRepository;
+
+    @Autowired
+    private SubassemblyService subassemblyService;
 
     private Long machineIds;
 
@@ -119,11 +126,53 @@ public class MachineEndToEndTest {
 
     @Test
     @Transactional
+    public void shouldCreateMachine() throws Exception {
+        Part cpu = partRepository.save(new Part("CPU2", 10002L));
+        Part cpuCooler = partRepository.save(new Part("CPU Cooler2", 1002L));
+        Part thermalPaste = partRepository.save(new Part("Thermal Paste2", 102L));
+        List<Part> cpuParts = Arrays.asList(cpu, cpuCooler, thermalPaste);
+
+        Subassembly cpuSubassembly = subassemblyService.createSubassembly(new Subassembly("CPU2", cpuParts));
+        List<Subassembly> subassemblies = Arrays.asList(cpuSubassembly);
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objectNode = mapper.createObjectNode();
+        objectNode.put("machineName", "Windows Desktop2");
+        objectNode.set("subassemblies", mapper.valueToTree(subassemblies));
+
+        MvcResult result = mockMvc.perform(post("/api/machine/add")
+                        .contentType("application/json")
+                        .content(objectNode.toString())
+                        .accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.machineName").value("Windows Desktop2"))
+                .andExpect(jsonPath("$.subassemblies[0].subassemblyName").value("CPU2"))
+                .andExpect(jsonPath("$.subassemblies[0].subassemblyPrice").value(11106L))
+                .andExpect(jsonPath("$.subassemblies[0].parts[0].partName").value("CPU2"))
+                .andExpect(jsonPath("$.subassemblies[0].parts[1].partName").value("CPU Cooler2"))
+                .andExpect(jsonPath("$.subassemblies[0].parts[1].partPrice").value(1002L))
+                .andExpect(jsonPath("$.subassemblies[0].parts[2].partName").value("Thermal Paste2"))
+                .andExpect(jsonPath("$.subassemblies[0].parts[2].partPrice").value(102L))
+                .andReturn();
+
+        String jsonString = result.getResponse().getContentAsString();
+        JsonNode machineJson = new ObjectMapper().readTree(jsonString);
+        Long id = machineJson.path("id").asLong();
+        Optional<Machine> entity = machineRepository.findById(id);
+        assertTrue(entity.isPresent());
+
+        Machine machineEntity = entity.get();
+        assertEquals("Windows Desktop2", machineEntity.getMachineName());
+        assertEquals("CPU2", machineEntity.getSubassemblies().get(0).getSubassemblyName());
+    }
+
+    @Test
+    @Transactional
     public void shouldUpdateMachineName() throws Exception {
         mockMvc.perform(put("/api/machine/" + machineIds + "/update/name")
-                .contentType("application/json")
-                .content("{\"machineName\":\"MacBook\"}")
-                .accept("application/json"))
+                        .contentType("application/json")
+                        .content("{\"machineName\":\"MacBook\"}")
+                        .accept("application/json"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.machineName").value("MacBook"));
     }
