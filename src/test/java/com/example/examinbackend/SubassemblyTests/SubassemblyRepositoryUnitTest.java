5e25d2f4b8d5904c76780dd0928ff4593cb78fed
william vage <william.vage@gmail.com>
2023-12-17 20:06:10 +0100

    Added @ActiveProfiles annotation to SubassemblyRepositoryUnitTest

--- src/test/java/com/example/examinbackend/SubassemblyTests/SubassemblyRepositoryUnitTest.java
+++ src/test/java/com/example/examinbackend/SubassemblyTests/SubassemblyRepositoryUnitTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class SubassemblyRepositoryUnitTest {
     @Autowired
     private SubassemblyRepository subassemblyRepository;
