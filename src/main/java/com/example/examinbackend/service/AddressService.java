74bdeff3df46a502cd410c8af2b8f3e4f658666c
Stefan <stefan.naeve@hotmail.com>
2023-12-19 00:17:40 +0100

    Made a add customer to address in addressService

--- src/main/java/com/example/examinbackend/service/AddressService.java
+++ src/main/java/com/example/examinbackend/service/AddressService.java
@@ -1,22 +1,26 @@
 package com.example.examinbackend.service;
 
 import com.example.examinbackend.model.Address;
+import com.example.examinbackend.model.Customer;
 import com.example.examinbackend.repository.AddressRepository;
+import com.example.examinbackend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class AddressService {
 
     private final AddressRepository addressRepository;
 
+    private final CustomerRepository customerRepository;
+
     @Autowired
-    public AddressService(AddressRepository addressRepository) {
+    public AddressService(AddressRepository addressRepository, CustomerRepository customerRepository) {
         this.addressRepository = addressRepository;
+        this.customerRepository = customerRepository;
     }
 
     public Optional<Address> getAddressById(Long addressId) {
@@ -42,5 +46,31 @@ public class AddressService {
 
     public Optional<Address> createAddress(Address address) {
         return Optional.of(addressRepository.save(address));
+    }
+
+    public Optional<Address> addCustomerToAddress(Long addressId, Long customerId) {
+        Optional<Address> optionalAddress = getAddressById(addressId);
+        Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+        if (optionalAddress.isEmpty() || optionalCustomer.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Address address = optionalAddress.get();
+        Customer customer = optionalCustomer.get();
+
+        List<Address> addresses = customer.getAddresses();
+        if(addresses == null) {
+            addresses = new ArrayList<>();
+        }
+        addresses.add(address);
+        customer.setAddresses(addresses);
+        customerRepository.save(customer);
+        List<Customer> customers = address.getCustomer();
+        if (customers == null) {
+            customers = new ArrayList<>();
+        }
+        customers.add(optionalCustomer.get());
+        optionalAddress.get().setCustomer(customers);
+        return Optional.of(addressRepository.save(optionalAddress.get()));
     }
 }
